303489bc4e0be04fd4703a008e729da8697ad085
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2024-10-18 10:02:22 +0200

    Add main plugin source file.

---
+++ prep_eps.c
@@ -0,0 +1,75 @@
+#include <slurm/slurm.h>
+#include <slurm/slurm_errno.h>
+
+#include "src/interfaces/prep.h"
+
+#define P_NAME "PrEp-EPS: "
+
+const char plugin_name[] = "EPS PrEp plugin";
+const char plugin_type[] = "prep/eps";
+const uint32_t plugin_version = SLURM_VERSION_NUMBER;
+
+//////////////////////////////
+// Slurm Plugin API hooks
+/////////////////////////////
+
+extern int init(void)
+{
+        slurm_info("Init: %s", plugin_name);
+	return SLURM_SUCCESS;
+}
+
+extern void fini(void)
+{
+        slurm_info("Fini: %s", plugin_name);
+}
+
+extern void prep_p_register_callbacks(prep_callbacks_t* callbacks) {}
+
+extern int prep_p_prolog(job_env_t* job_env, slurm_cred_t *cred)
+{
+        slurm_info("Prolog: %s", plugin_name);
+	return SLURM_SUCCESS;
+}
+
+extern int prep_p_epilog(job_env_t* job_env, slurm_cred_t *cred)
+{
+        slurm_info("Epilog: %s", plugin_name);
+	return SLURM_SUCCESS;
+}
+
+extern int prep_p_prolog_slurmctld(job_record_t* job_ptr, bool* async)
+{
+        slurm_info("Ctld_prolog: %s", plugin_name);
+	return SLURM_SUCCESS;
+}
+
+extern int prep_p_epilog_slurmctld(job_record_t* job_ptr, bool* async)
+{
+        slurm_info("Ctld_epilog: %s", plugin_name);
+	return SLURM_SUCCESS;
+}
+
+extern void prep_p_required(prep_call_type_t type, bool* required)
+{
+    *required = false;
+    switch (type)
+    {
+        case PREP_PROLOG_SLURMCTLD:
+            if (running_in_slurmctld())
+                *required = true;
+            break;
+        case PREP_EPILOG_SLURMCTLD:
+            if (running_in_slurmctld())
+                *required = false;
+            break;
+        case PREP_PROLOG:
+        case PREP_EPILOG:
+            if (running_in_slurmd())
+                *required = false;
+            break;
+        default:
+            return;
+    }
+    return;
+}
