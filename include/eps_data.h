bc647cf66d437a4ee4a00e6ba673b39895ebb2d3
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2024-11-11 13:34:29 +0100

    Add eps_data translation unit.

---
+++ include/eps_data.h
@@ -0,0 +1,63 @@
+#ifndef _EPS_H
+#define _EPS_H
+
+#include <EMA.h>
+
+#include "eps_resources.h"
+
+#include "src/slurmd/slurmd/slurmd.h"
+#include "src/slurmctld/slurmctld.h"
+
+
+typedef struct eps_meta_data
+{
+    int jobid;
+    int userid;
+    int nnodes;
+    char* tstart;
+    eps_resources_t* resources;
+} eps_meta_data_t;
+
+typedef struct eps_job_data
+{
+    int jobid;
+    unsigned long long int energy;
+    char* tstart;
+    int duration;
+} eps_job_data_t;
+
+typedef struct eps_device_data
+{
+    int jobid;
+    char* nodename;
+    unsigned long long int energy;
+    char* tstart;
+    unsigned long long duration;
+    char* device;
+    void* device_type; /* EMA plugin type? */
+    // TODO: Clarify what this should be...
+    void* resource;
+    int exclusive;
+} eps_device_data_t;
+
+eps_meta_data_t* get_metadata(const job_record_t* job);
+void free_metadata(eps_meta_data_t* data);
+void log_metadata(eps_meta_data_t* data);
+
+eps_job_data_t* get_job_data(
+    const job_record_t* job, unsigned long long energy);
+void free_job_data(eps_job_data_t* data);
+void log_job_data(eps_job_data_t* data);
+
+eps_device_data_t* get_device_data(
+    const Device* device,
+    const job_env_t* job,
+    unsigned long long energy,
+    const time_t* ts,
+    unsigned long long tstart,
+    unsigned long long tend
+);
+void free_device_data(eps_device_data_t* data);
+void log_device_data(eps_device_data_t* data);
+
+#endif
---
+++ src/eps_data.c
@@ -0,0 +1,119 @@
+#include <eps_data.h>
+#include <eps_utils.h>
+
+#include <limits.h>
+
+#include <slurm/slurm.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+eps_meta_data_t* get_metadata(const job_record_t* job)
+{
+    eps_meta_data_t* metadata = malloc(sizeof(eps_meta_data_t));
+
+    metadata->jobid = job->job_id;
+    metadata->userid = job->user_id;
+
+    metadata->nnodes = job->node_cnt;
+
+    metadata->tstart = to_string(&job->start_time);
+
+    metadata->resources = get_resources(job);
+
+    return metadata;
+}
+
+void free_metadata(eps_meta_data_t* data)
+{
+    free(data->resources);
+    free(data->tstart);
+    free(data);
+}
+
+void log_metadata(eps_meta_data_t* data)
+{
+    slurm_info("Job ID: %d", data->jobid);
+    slurm_info("User ID: %d", data->userid);
+    slurm_info("Nodes count: %d", data->nnodes);
+    slurm_info("Timestamp: %s", data->tstart);
+    log_resources(data->resources);
+}
+
+eps_job_data_t* get_job_data(
+    const job_record_t* job, unsigned long long energy)
+{
+    eps_job_data_t* data = malloc(sizeof(eps_job_data_t));
+
+    data->jobid = job->job_id;
+    data->energy = energy;
+    data->tstart = to_string(&job->start_time);
+    data->duration = job->end_time - job->start_time;
+
+    return data;
+}
+
+void free_job_data(eps_job_data_t* data)
+{
+    free(data->tstart);
+    free(data);
+}
+
+void log_job_data(eps_job_data_t* data)
+{
+    slurm_info("Job ID: %d", data->jobid);
+    slurm_info("Energy: %lld", data->energy);
+    slurm_info("Time started: %s", data->tstart);
+    slurm_info("Duration: %d", data->duration);
+}
+
+eps_device_data_t* get_device_data(
+    const Device* device,
+    const job_env_t* job,
+    unsigned long long energy,
+    const time_t* tstart,
+    unsigned long long tstart_us,
+    unsigned long long tend_us
+)
+{
+    eps_device_data_t* data = malloc(sizeof(eps_device_data_t));
+
+    data->jobid = job->jobid;
+
+    char nodename[HOST_NAME_MAX];
+    int err = gethostname(nodename, HOST_NAME_MAX);
+    data->nodename = err ? NULL : nodename;
+
+    data->device = (char*)EMA_get_device_name(device);
+    //TODO: Realize device type extraction...
+    data->device_type = NULL;
+
+    data->energy = energy;
+
+    data->tstart = to_string(tstart);
+    data->duration = tend_us - tstart_us;
+
+    // TODO: Find a way to write something meaningfull here...
+    data->resource = NULL;
+    data->exclusive = 0;
+
+    return data;
+}
+
+void free_device_data(eps_device_data_t* data)
+{
+    // TODO: Remove the latch here later...
+    if (data->resource) free(data->resource);
+    free(data->tstart);
+    free(data);
+}
+
+void log_device_data(eps_device_data_t* data)
+{
+    slurm_info("Job ID: %d", data->jobid);
+    slurm_info("Node name: %s", data->nodename);
+    slurm_info("Device: %s", data->device);
+    slurm_info("Energy: %lld", data->energy);
+    slurm_info("Time started: %s", data->tstart);
+    slurm_info("Duration: %lld", data->duration);
+}
