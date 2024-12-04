618718883ea85a5c018975a3f05c63d1cad33050
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2024-12-04 09:19:44 +0100

    Set up minimal example of using fork in local context of init spank hook.

--- eps.c
+++ eps.c
@@ -1,8 +1,14 @@
 #include <slurm/spank.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
 
 #define PLUGIN_NAME "Spank/Eps"
 
 SPANK_PLUGIN(eps, 1)
+
+const char* global = "global";
 
 /********************************
  *
@@ -11,6 +17,34 @@ SPANK_PLUGIN(eps, 1)
  ********************************/
 int slurm_spank_init(spank_t sp, int ac, char **av) {
     slurm_info("Init: " PLUGIN_NAME);
+    pid_t hook_pid = getpid();
+
+    slurm_info("PID: %d", hook_pid);
+    slurm_info("Global: %s", global);
+
+    if (spank_context() == S_CTX_LOCAL) {
+        pid_t pid = fork();
+
+        switch(pid) {
+            case -1:
+                perror("fork");
+                return 1;
+            case 0:
+                pid_t child_pid = getpid();
+                pid_t parent_pid = getppid();
+                slurm_info("Child PID: %d", child_pid);
+                slurm_info("Parent PID: %d", parent_pid);
+                slurm_info("Global: %s", global);
+                slurm_info("Child exiting success...");
+                exit(EXIT_SUCCESS);
+            default:
+                slurm_info("Child PID: %d", pid);
+                slurm_info("Init hook exits...");
+                return 0;
+        }
+
+    }
+
     return 0;
 }
 
