19f458139404da77e9f064acfb67ae776bef1d15
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2024-12-06 10:15:04 +0100

    Realize sockets sync with semaphores in efp test.

--- __test__/efp.c
+++ __test__/efp.c
@@ -1,8 +1,14 @@
+#include <fcntl.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include <eps_socket.h>
+
+#define SEM_NAME "/efpsem"
 
 uint32_t mock_jid = 42;
 
@@ -10,6 +16,8 @@ static eps_sockaddr_t saddr, caddr;
 int ssfd, csfd;
 
 char* spath;
+
+sem_t* mutex;
 
 void efp_main() {
     printf("[EFP]: Started!\n");
@@ -28,14 +36,30 @@ void efp_main() {
 
     printf("[EFP]: ssfd=%d\n", ssfd);
 
-    sleep(10);
+    int sem_val;
+    sem_getvalue(mutex, &sem_val);
+    printf("[EFP]: Mutex: %d\n", sem_val);
+    if (sem_post(mutex) == -1) goto efp_exit;
+    sem_getvalue(mutex, &sem_val);
+    printf("[EFP]: Mutex: %d\n", sem_val);
+
+    /* WARN: Blocking call! */
+    int acsfd = accept(ssfd, NULL, NULL);
+    printf("[EFP]: Accepted socket fd = %d\n", acsfd);
+
+    
+efp_exit:
+
+    printf("[EFP]: Unlinking spath...\n");
+    if (unlink(spath) == -1)
+        perror("[EFP] unlink");
+
+    if (spath) free(spath);
 
     printf("[EFP]: Closing ssdf...\n");
     if (close(ssfd) == -1)
         perror("[EFP] close");
-    
-efp_exit:
-    if (spath) free(spath);
+
     printf("[EFP]: Exiting...\n");
     exit(0);
 }
@@ -61,6 +85,12 @@ void main() {
         exit(1);
     }
 
+    printf("[main]: Opening shared semaphore...\n");
+    if ((mutex = sem_open(SEM_NAME,O_CREAT,S_IRWXU,0)) == SEM_FAILED) {
+        perror("[main]: sem_open");
+        goto main_exit;
+    }
+
     pid_t pid = fork();
 
     switch(pid) {
@@ -72,12 +102,17 @@ void main() {
         default:
             printf("[main]: Process Group ID: %d\n", getpgid(getpid()));
             printf("[main]: Socket Path: %s\n", spath);
-            printf("[main]: Sleeping...\n");
-            sleep(3);
+            printf("[main]: Waiting for server socket...\n");
+            int sem_val;
+            sem_getvalue(mutex, &sem_val);
+            printf("[main]: Mutex: %d\n", sem_val);
+            if (sem_wait(mutex) == -1) goto main_exit;
+            sem_getvalue(mutex, &sem_val);
+            printf("[main]: Mutex: %d\n", sem_val);
             printf("[main]: Getting client socket fd...\n");
             csfd = getcsfd(&caddr, spath);
 
-            if (ssfd == -1) {
+            if (csfd == -1) {
                 printf("[main]: Failed to get client socket fd!\n");
                 goto main_exit;
             }
@@ -90,7 +125,13 @@ void main() {
     }
 
 main_exit:
+
     if (spath) free(spath);
+
+    printf("[main]: Unlinking semaphore...\n");
+    if (sem_unlink(SEM_NAME) == -1)
+        perror("[main]: sem_unlink");
+
     printf("[main]: Exiting...\n");
     exit(0);
 }
