88cc77b695947b8addf9617486102a333a49a236
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2024-12-10 10:23:35 +0100

    Utilize shared memory to pass EFP pid from init to exit hook.

--- eps.c
+++ eps.c
@@ -9,6 +9,7 @@
 #include <eps_utils.h>
 #include <eps_sem.h>
 #include <eps_efp.h>
+#include <eps_shm.h>
 
 #define PLUGIN_NAME "Spank/Eps"
 
@@ -39,6 +40,20 @@ int slurm_spank_task_init_privileged(spank_t sp, int ac, char **av) {
     sprintf(msg, "Hook PID: %d\n", hook_pid);
     log_message(msg, log_fd);
 
+    sprintf(msg, "Initializing shared memory...\n");
+    log_message(msg, log_fd);
+    int shmfd;
+    char* shm_name = get_shared_memory_region_name(jid);
+
+    unlink_shared_memory_region(shm_name);
+    pid_t* efp_pid = (pid_t*)get_shared_memory_addr(shm_name, sizeof(pid_t*), &shmfd);
+    if (!efp_pid) {
+        sprintf(msg, "error: get_shared_memory_addr: %s\n", strerror(errno));
+        log_message(msg, log_fd);
+        return 1;
+    }
+    free(shm_name);
+
     pid_t pid = fork();
     switch(pid) {
         case -1:
@@ -51,8 +66,15 @@ int slurm_spank_task_init_privileged(spank_t sp, int ac, char **av) {
             sprintf(msg, "Child PID: %d\n", pid);
             log_message(msg, log_fd);
 
-            sprintf(msg, "Child Process GID: %d\n", getpgid(pid));
+            sprintf(msg, "Writing EFP's PID to shared memory\n");
             log_message(msg, log_fd);
+            *efp_pid = pid;
+
+            int err =  discard_shared_memory_addr((void*)efp_pid, sizeof(pid_t*), &shmfd);
+            if (err) {
+                sprintf(msg, "error: discard_shared_memory_addr: %s\n", strerror(errno));
+                log_message(msg, log_fd);
+            }
 
             sprintf(msg, "Init hook exits...\n");
             log_message(msg, log_fd);
@@ -84,6 +106,30 @@ int slurm_spank_task_exit(spank_t sp, int ac, char **av) {
     sprintf(msg, "Hook PID: %d\n", hook_pid);
     log_message(msg, log_fd);
 
+    sprintf(msg, "Obtaining shared memory address...\n");
+    log_message(msg, log_fd);
+    char* shm_name = get_shared_memory_region_name(jid);
+
+    int shmfd = open_shared_memory_region(shm_name);
+    if (shmfd == -1) {
+        sprintf(msg, "error: open_shared_memory_region: %s", strerror(errno));
+        log_message(msg, log_fd);
+        // To return or not to return ???
+    }
+    free(shm_name);
+
+    pid_t* efp_pid = (pid_t*)map_shared_memory_region(shmfd, sizeof(pid_t*));
+    if (!efp_pid) {
+        sprintf(msg, "error: map_shared_memory_region: %s", strerror(errno));
+        log_message(msg, log_fd);
+        // To return or not to return ???
+    }
+
+    sprintf(msg, "Reading shared memory...\n");
+    log_message(msg, log_fd);
+    sprintf(msg, "EFP's PID: %d...\n", *efp_pid);
+    log_message(msg, log_fd);
+
     sprintf(msg, "Obtaining semaphore...\n");
     log_message(msg, log_fd);
     sem_t* mutex = get_efp_mutex(sem_name, 0);
@@ -109,6 +155,11 @@ int slurm_spank_task_exit(spank_t sp, int ac, char **av) {
     sprintf(msg, "Waiting for EFP to finish or fail...\n");
     log_message(msg, log_fd);
     sleep(3);
+
+    sprintf(msg, "Cleaning up shared memory...\n");
+    log_message(msg, log_fd);
+    unmap_shared_memory_region((void*)efp_pid, sizeof(pid_t*));
+    close_shared_memory_region(shmfd);
 
     close(log_fd);
 
