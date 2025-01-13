dfd0c4174a1ad3667598668d4300504450e1a808
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2025-01-13 11:01:30 +0100

    Remove unneeded close_shared_memory_region calls.

--- eps.c
+++ eps.c
@@ -65,7 +65,6 @@ int slurm_spank_task_init_privileged(spank_t sp, int ac, char **av) {
         if (err) {
             LOG(log_fd, "error: discard_shared_memory_addr: %s", strerror(errno));
         }
-        close_shared_memory_region(shmfd);
         LOG(log_fd, "error: get_efp_sem: %s", strerror(errno));
         fclose(log_fd);
         return 1;
@@ -78,7 +77,6 @@ int slurm_spank_task_init_privileged(spank_t sp, int ac, char **av) {
             if (err) {
                 LOG(log_fd, "error: discard_shared_memory_addr: %s", strerror(errno));
             }
-            close_shared_memory_region(shmfd);
             sem_close(proceed_init);
             sem_unlink(sem_name);
             free(sem_name);
@@ -93,7 +91,6 @@ int slurm_spank_task_init_privileged(spank_t sp, int ac, char **av) {
             if (err) {
                 LOG(log_fd, "error: discard_shared_memory_addr: %s", strerror(errno));
             }
-            close_shared_memory_region(shmfd);
             sem_close(proceed_init);
             free(sem_name);
             fclose(log_fd);
@@ -117,7 +114,6 @@ int slurm_spank_task_init_privileged(spank_t sp, int ac, char **av) {
             if (err) {
                 LOG(log_fd, "error: discard_shared_memory_addr: %s", strerror(errno));
             }
-            close_shared_memory_region(shmfd);
 
             LOG(log_fd, "Closing semaphore...");
             sem_close(proceed_init);
