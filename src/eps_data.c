b29e5a4da286d96ee7256d2dd1f00826f1320122
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2025-03-04 08:26:45 +0100

    Add job name extraction and saving to db for prep plugin.

--- src/eps_data.c
+++ src/eps_data.c
@@ -1,13 +1,24 @@
 #include <stdlib.h>
+#include <limits.h>
 
 #include <eps_data.h>
+
 
 
 eps_allocation_data_t* get_allocation_data(const job_record_t* job)
 {
     eps_allocation_data_t* data = malloc(sizeof(eps_allocation_data_t));
 
+    char job_name[NAME_MAX];
+
+    if (!job->name) {
+        snprintf(job_name, NAME_MAX, "job%d", job->job_id);
+    } else {
+        snprintf(job_name, NAME_MAX, "%s", job->name);
+    }
+
     data->jobid = job->job_id;
+    data->jobname = strdup(job_name);
     data->userid = job->user_id;
     data->nnodes = job->node_cnt;
     // TODO: Save it in the right format (ts + tz_info, maybe as string)...
@@ -18,6 +29,7 @@ eps_allocation_data_t* get_allocation_data(const job_record_t* job)
 
 void free_allocation_data(eps_allocation_data_t* data)
 {
+    free(data->jobname);
     free(data);
 }
 
