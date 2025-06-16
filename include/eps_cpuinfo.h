5d3a17596316b6f5229f91e8eee9ff846cbcb73d
Serhii Yahdzhyiev <yahdzhyiev@perfacct.eu>
2025-06-16 09:39:53 +0200

    Remove internal functions of eps_cpuinfo from header.
    And make functions static.

--- include/eps_cpuinfo.h
+++ include/eps_cpuinfo.h
@@ -10,9 +10,6 @@ typedef struct {
     int* socket_idx;
 } eps_cpuinfo_t;
 
-int get_sockets_count(hwloc_topology_t topology);
-int get_cores_count(hwloc_topology_t topology);
-
 int populate_cpuinfo(hwloc_topology_t topology, eps_cpuinfo_t* info);
 int map_cpuinfo(eps_cpuinfo_t* info, int fd);
 
--- src/eps_cpuinfo.c
+++ src/eps_cpuinfo.c
@@ -3,12 +3,14 @@
 #include <eps_shm.h>
 #include <eps_cpuinfo.h>
 
-int get_sockets_count(hwloc_topology_t topology){
+static int _get_sockets_count(hwloc_topology_t topology)
+{
     int count = hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_PACKAGE);
     return count;
 }
 
-int get_cores_count(hwloc_topology_t topology) {
+static int _get_cores_count(hwloc_topology_t topology)
+{
     int count = hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_CORE);
     return count;
 }
