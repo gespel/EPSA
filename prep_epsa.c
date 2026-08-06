07048d09bd73190ccd26335f4259426810016c92
gespel <sten.heimbrodt@hotmail.de>
2026-08-06 19:33:52 +0200

    Fix unused `uname` variable and remove redundant free call
    Remove cached user name cache entry

--- prep_epsa.c
+++ prep_epsa.c
@@ -368,10 +368,9 @@ extern int prep_p_prolog_slurmctld(job_record_t* job_ptr, bool* async)
 
     slurm_info("Ctld_prolog: %s", plugin_name);
     slurm_info("Job Id: %u", job_ptr->job_id);
-    
+
     char *uname = uid_to_string_cached(job_ptr->user_id);
     slurm_info("User: %s", uname);
-    free(uname);
 
     slurm_info("Collecting job allocation data...");
     eps_allocation_data_t data;
